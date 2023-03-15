6938f3145675bb180332819bcb2678dc424fed7a
rishikeshsk123 <rishikeshsk007@gmail.com>
2023-03-15 11:59:49 -0400

    Budget app edited

--- src/App.js
+++ src/App.js
@@ -9,27 +9,47 @@ import Budget from './components/Budget';
 
 
 import { AppProvider } from './context/AppContext';
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
+import AllocationForm from './components/AllocationForm';
 const App = () => {
     return (
         <AppProvider>
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
-                    <div className='row mt-3'>
-                        {/* Add Budget component here under */}        
+                <div className='row mt-3'>
+                    {/* Add Budget component here under */}
+                    <div className='col-sm'>
+                        <Budget />
+                    </div>
+                    {/* Add Remaining component here under */}
+                    <div className='col-sm'>
+                        <Remaining />
+                    </div>
+                    {/* Add ExpenseTotal component here under */}
+                    <div className="col-sm">
+                        <ExpenseTotal />
+                    </div>
+                </div>
 
-                        {/* Add Remaining component here under */}        
+                <h3 className="mt-3">Allocation</h3>
+                <div className="row">
+                    {/* Add ExpenseList component here under */}
+                    <div className="col-sm">
+                        <ExpenseList />
+                    </div>
+                </div>
 
-                        {/* Add ExpenseTotal component here under */}        
-                       
-                        {/* Add ExpenseList component here under */}         
-
-                        {/* Add ExpenseItem component here under */}        
-
-                        {/* Add AllocationForm component here under */}        
-
+                <h3 className="mt-3">Change</h3>
+                <div className="row">
+                    {/* Add AllocationForm component here under */}
+                    <div className="col-sm">
+                        <AllocationForm />
+                    </div>
                 </div>
             </div>
-        </AppProvider>
+        </AppProvider >
     );
 };
 export default App;
--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -1,1 +1,79 @@
+import { useContext, useState } from "react";
+import { AppContext } from "../context/AppContext";
 
+const AllocationForm = (props) => {
+    const { dispatch, remaining } = useContext(AppContext);
+
+    const [name, setName] = useState('');
+    const [cost, setCost] = useState('');
+    const [action, setAction] = useState('');
+
+    const submitEvent = ()=>{
+        if(cost > remaining){
+            alert("The value cannot exceed remaining funds  £"+remaining);
+            setCost('');
+            return;
+        }
+
+        const expense = {
+            name: name,
+            cost: parseInt(cost),
+        };
+        if(action === "Reduce"){
+            dispatch({
+                type: 'RED_EXPENSE',
+                payload: expense,
+            });
+        } else {
+            dispatch({
+                type: 'ADD_EXPENSE',
+                payload: expense,
+            });
+        }
+    };
+
+    return(
+        <div>
+            <div className="row">
+                
+                <div className="input-group mb-3" style={{marginLeft: '2rem'}}>
+                    <div className="input-group-prepend">
+                        <label htmlFor="inputGroupDelect01" className="input-group-text">Department</label>
+                    </div>
+                    <select id="inputGroupSelect01" className="custom-select" onChange={(event)=> setName(event.target.value)}>
+                        <option defaultValue>Choose...</option>
+                        <option value="Marketing" name='marketing'>Marketing</option>
+                        <option value="Sales" name='sales'>Sales</option>
+                        <option value="Finance" name="finance">Finance</option>
+                        <option value="HR" name="hr">HR</option>
+                        <option value="IT" name="it">IT</option>
+                        <option value="Admin" name="admin">Admin</option>
+                    </select>
+
+                    <div className="input-group-prepend" style={{marginLeft: '2rem'}}>
+                        <label htmlFor="inputGroupSelect02" className="input-group-text">Allocation</label>
+                    </div>
+                    <select id="inputGroupSelect02" onChange={(event)=> setAction(event.target.value)} className="custom-select">
+                        <option defaultValue value="Add" name="Add">Add</option>
+                        <option value="Reduce" name="Reduce">Reduce</option>
+                    </select>
+
+                    <input
+                        required='required'  
+                        type='number'
+                        id='cost'
+                        value={cost}
+                        style={{marginLeft: '2rem', size: 10}}
+                        onChange={(event)=>setCost(event.target.value)}
+                    ></input>
+
+                    <button className="btn btn-primary" onClick={submitEvent} style={{marginLeft: '2rem'}}>
+                        Save
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AllocationForm;
